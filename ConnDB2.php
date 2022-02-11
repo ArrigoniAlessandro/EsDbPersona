46eb20efaad1b20a67218d6953616788a2165189
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2022-02-11 15:54:32 +0100

    Added ConnDB

---
+++ ConnDB2.php
@@ -0,0 +1,57 @@
+<htlm>
+    <body>
+
+        <form method = "POST">        
+            <fieldset>
+                <legend>
+                    Inserimento dati nel DataBase [PERSONA]:
+                </legend>
+                <p>CF: <input type="text" name="cf"></p>
+                <p>Nome: <input type="text" name="nome"></p>
+                <p>Cognome: <input type="text" name="cognome"></p>
+                <p>Eta: <input type="text" name="eta"></p>
+                <input type="submit" value="Invio dati">
+            </fieldset>
+        </form>
+
+</htlm>
+
+<?php
+
+    include "FunConnDB.php";
+    
+    include "conf.php";
+
+    if(!empty($_POST)){
+
+        $cf = $_POST['cf'];
+        $nome = $_POST['nome'];
+        $cognome = $_POST['cognome'];
+        $eta = $_POST['eta'];
+
+        $conn = connessioneDB($hostname, $user, $pw, $db);
+
+        if ($conn -> connect_error) { 
+            
+            die('CONNESSIONE FALLITA: '  . $conn->connect_error); 
+
+        }else{ 
+
+            $sql = "INSERT INTO persona (cf, nome, cognome, eta)
+            VALUES ('$cf', '$nome', '$cognome', '$eta')";
+            
+            if ($conn -> query($sql) === TRUE) {
+
+              echo "OPERAZIONE ESEGUITA CON SUCCESSO!";
+
+            } else {
+
+              echo "ERRORE: " . $sql . "<br>" . $conn->error;
+            }
+            
+            $conn -> close();
+
+        }
+    }
+
+?>
---
+++ FunConnDB.php
@@ -0,0 +1,10 @@
+<?php
+
+    function connessioneDB($servername, $username, $password, $nameDB){
+
+        $conn = new mysqli($servername, $username, $password, $nameDB); 
+        return $conn;
+        
+    }
+
+?>
